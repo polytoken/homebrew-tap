71dfcd8c0527a8e0b980a56fd5da53ce528f8bc1
Ed Ropple <ed@edropple.com>
2026-06-30 17:02:13 -0400

    polytoken 0.4.0-unstable.3

--- Formula/polytoken-unstable.rb
+++ Formula/polytoken-unstable.rb
@@ -1,22 +1,22 @@
 class PolytokenUnstable < Formula
   desc "AI agent harness"
   homepage "https://polytoken.dev"
-  version "0.4.0-unstable.2"
+  version "0.4.0-unstable.3"
   license :cannot_represent
   conflicts_with "polytoken", because: "both install the polytoken executable"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.2/macos-arm64/polytoken.tar.gz"
-    sha256 "b7356f3544f7c8384c43c64e3d817882ecf954589e689abfa8eebece98c90b7e"
+    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.3/macos-arm64/polytoken.tar.gz"
+    sha256 "2f7c41383e1889c6a2fa0d23e94c82e9a8cd77df9a3421eaf85386ec9b923386"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.2/macos-amd64/polytoken.tar.gz"
-    sha256 "7287b301bdf280ba7908d4dea7ea8a6d57ad2f02d914bd1424ffe74f8b76fa12"
+    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.3/macos-amd64/polytoken.tar.gz"
+    sha256 "27bed0639cb596ab3548ae2de3adb52e7ffbaa9c9ff604e13ff17271005bd3d2"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.2/linux-arm64/polytoken.tar.gz"
-    sha256 "84b5e1457f89eb582917116aa66593691fa72ad14ecfc3dc521fcf43a9912d43"
+    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.3/linux-arm64/polytoken.tar.gz"
+    sha256 "de1ff08ef0ee49792dd909422d055fd539c9f0690b1ca6658c38486528f987de"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.2/linux-amd64/polytoken.tar.gz"
-    sha256 "ae4ba26ef9a7ef53a07270fd6f0d591afea5973ceaa44e704b1fc7f652aae73c"
+    url "https://dl.polytoken.dev/unstable/0.4.0-unstable.3/linux-amd64/polytoken.tar.gz"
+    sha256 "468d6f3116ceb7ee3af65aeaa73a74ce745aeb78071519b12a86b071f25ca270"
   else
     odie "Unsupported platform"
   end
